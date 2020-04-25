11171b2573e506702b6eb6e153504ddc64cf07f4
Ronnie Simonelli <ronnie@seeclickfix.com>
2020-04-25 18:20:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RemoteImageView
+
+Use this to fetch and display an image in a SwiftUI view
---
+++ Tests/RemoteImageViewTests/RemoteImageViewTests.swift
@@ -0,0 +1,4 @@
+import XCTest
+@testable import RemoteImageView
+
+final class RemoteImageViewTests: XCTestCase { }
